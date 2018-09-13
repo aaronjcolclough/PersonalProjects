89100d27540f9ffabd3611efa79e926e58be6783
Aaron Colclough <aaron.j.colclough@outlook.com>
2018-09-12 21:15:36 -0400

    Sep 12 2018

---
+++ Python/Age.py
@@ -0,0 +1,6 @@
+# Write a program that tells the user when they will turn 100 years old.
+
+age = int (input("Enter your age:"))
+year = int (2018 - age) + 100
+
+print(f"You will be 100 in {year}")
