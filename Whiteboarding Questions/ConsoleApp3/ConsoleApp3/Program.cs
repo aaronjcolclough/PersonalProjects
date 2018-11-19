db97d32c944620978d803b5bf9c3e45aaf24d339
Aaron Colclough <aaron.j.colclough@outlook.com>
2018-11-19 09:34:55 -0500

    Nov 18 2018

--- Whiteboarding Questions/ConsoleApp3/ConsoleApp3/Program.cs
+++ Whiteboarding Questions/ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApp3
 {
@@ -6,37 +7,13 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            // create int array
-            int evencount = 0;
-            int oddcount = 0;
-            //int[] array = new int[100];
-            // fill array with numbers 1-100
-            for (int i = 1; i <= 100; i++)
-            {
-                //array[i] = i + 1;
+            Console.WriteLine(sum(999));
+        }
 
-                // display whether or not even or odd
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine($"{i} is Even");
-                    evencount++;
-                }
-                else
-                {
-                    Console.WriteLine($"{i} is Odd");
-                    oddcount++;
-                }
-            }
+        private static int sum(int v)
+        {
+            return v.ToString().Select(x => int.Parse(x.ToString())).ToArray().Sum(); ;
 
-            
-
-            // determine number is even or odd
-            //for (int j = 0; j < array.Length; j++)
-            {
-                
-            }
-            // display even and odd counts
-            Console.WriteLine($"Even number count is {evencount}. Odd number count is {oddcount}.");
         }
     }
 }
---
+++ Whiteboarding Questions/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FillList();
+            Console.WriteLine(LListAlgo(head, 5));
+        }
+
+        private static void FillList()
+        {
+            
+
+        }
+
+        private static bool LListAlgo(object head, int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static LinkedList<Node> list = new LinkedList<Node>();
+        
+    }
+    public class Node
+    {
+        int value;
+        Node next;
+    }
+
+    public class LinkedList
+    {
+        private 
+    }
+
+    
+}
