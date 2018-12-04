2b2e47295a24bd663bfa1eae12c723ee487578d5
Aaron Colclough <aaron.j.colclough@outlook.com>
2018-12-04 14:30:01 -0500

    Dec 4 2018

---
+++ Whiteboarding Questions/SQLite/SQLite/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLite
+{
+    class Program
+    {
+        public static SQLiteConnection m_dbConnection;
+
+        static void Main(string[] args)
+        {
+            CreateDb();
+            ConnectDb();
+            CreateTable();
+            FillTable();
+            QueryDb();
+            DropTable();
+            m_dbConnection.Close();
+        }
+
+        private static void DropTable()
+        {
+            string sql = "drop table cars";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"Name: {reader["name"]} " + $"Year: {reader["year"]}");
+            }
+        }
+
+        private static void QueryDb()
+        {
+            string sql = "select * from cars order by year desc";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"Name: {reader["name"]} " + $"Year: {reader["year"]}");
+            }
+        }
+
+        private static void FillTable()
+        {
+            string sql = "insert into cars (name, year) values ('Mustang', 1966)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into cars (name, year) values ('Camaro', 1969)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into cars (name, year) values ('Corvette', 1972)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        //create table
+        private static void CreateTable()
+        {
+            string sql = "create table cars (name varchar(20), year int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        //connect to database
+        private static void ConnectDb()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        //create database
+        private static void CreateDb()
+        {
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+        }
+    }
+}
--- Whiteboarding Questions/playpen/playpen/Program.cs
+++ Whiteboarding Questions/playpen/playpen/Program.cs
@@ -2,146 +2,32 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Diagnostics;
 
 namespace playpen
 {
     class Program
     {
+        public static Stopwatch stopWatch = new Stopwatch();
+        public static double pi;
         static void Main(string[] args)
         {
-            /*
-            string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
-            Console.WriteLine(dirReduc(a));
+            pi = CalcPi();
             
-            //Console.WriteLine(thirt(1234567));
-            //Console.WriteLine(thirt(87));
-            int[] a = { 2, 4 };
-            int[] b = { 1, 2, 2, 2, 3, 3, 4 };
-            Diff(a,b);
-            
-            */
+            Console.WriteLine($"Pi is: {pi} and was calculated in {stopWatch.Elapsed}");
+            Console.WriteLine(Math.PI);
         }
-        /*
-        private static void Diff(int[] a, int[] b)
+
+        private static double CalcPi()
         {
-            //List<int> bar = b.ToList();
-            int[] c = b.Except(a).ToArray();
-            foreach (int i in c)
+            stopWatch.Start();
+            for (int i = 1; pi != Math.PI; i += 4)
             {
-                Console.WriteLine(i);
+                pi += (4.0 / i) - (4.0 / (i + 2));
             }
+            stopWatch.Stop();
+            return pi;
         }
-        */
-        /*public static string Compression(string v)
-        {
-            string result = "";
-            int count = 1;
-            for (int i = 0, j = 1; j < v.Length; i++, j++)
-            {
-                if (v[i] == v[j])
-                {
-                    count++;
-                    if (j == v.Length - 1)
-                    {
-                        result = string.Format("{0}{1}", result, $"{v[j]}{count}");
-                    }
-                }
-                else
-                {
-                    result = string.Format("{0}{1}", result, $"{v[i]}{count}");
-                    count = 1;
-                    if (j == v.Length - 1)
-                    {
-                        result = string.Format("{0}{1}", result, $"{v[j]}{count}");
-                    }
-                }
-            }
-            return result;
-        }
-
-        private static int thirt(int n)
-        {
-            int[] pows = { 1, 10, 9, 12, 3, 4 };
-            int total = 0;
-
-
-            do
-            {
-                int[] bar = n.ToString().ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
-
-                for (int i = bar.Length - 1, j = 0; i >= 0; i--, j++)
-                {
-                    if (j == pows.Length)
-                    {
-                        j = 0;
-                    }
-                    total += bar[i] * pows[j];
-                }
-            }
-            while (total != n);
-
-            return total;
-        }
-        */
-        /*
-        public static string[] dirReduc(String[] arr)
-        {
-            string[] spam 
-            List<string> foo = arr.ToList();
-            int current = 0;
-            do
-            {
-                if ((foo[0] == "NORTH" && foo[1] == "SOUTH") ||
-                    (foo[0] == "SOUTH" && foo[1] == "NORTH") ||
-                    (foo[0] == "EAST" && foo[1] == "WEST") ||
-                    (foo[0] == "WEST" && foo[1] == "EAST"))
-                {
-                    foo.Remove(foo[current]);
-                    foo.Remove(foo[current + 1]);
-                    Console.WriteLine(foo.Count());
-
-                }
-                else if ()
-                else                
-                    break;
-                
-            }
-            while (true);
-            string[] bar = foo.ToArray();
-            foreach (string i in bar)
-                Console.WriteLine(i);
-            return bar;
-        }
-        */
-
-
-
-
-        /*public static int Persistence(long n)
-        {
-            
-            int count = 0;
-            if (n < 9)
-            {
-                return count;
-            }
-            do
-            {
-                    count++;
-                    string foo = n.ToString();
-                    int[] bar = foo.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
-
-                    int total = 1;
-                    foreach (int i in bar)
-                    {
-                        total *= i;
-                    }
-                    n = total;
-            }
-            while (n > 9);
-            return count;
-        }
-        */
     }
 }
 
