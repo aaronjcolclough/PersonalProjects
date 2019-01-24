aebe828b8a26d1657e44bc22d280760aca48a8b2
Aaron Colclough <aaron.j.colclough@outlook.com>
2019-01-24 15:52:57 -0500

    24 Jan 2019

---
+++ Unity Projects/IntroToScripting/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+    public new string name = "Untitled";
+    public float speed;
+    public bool alive;
+    public int age = 10;
+    public int[] points;
+
+    void Awake()
+    {
+        print("Awake");
+    }
+    // Use this for initialization
+    void Start () {
+        print("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var pos = transform.position;
+
+        transform.position = new Vector3(pos.x + speed, pos.y, pos.z);
+	}
+}
---
+++ Unity Projects/RPG 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody2D>();
+
+        if (Input.anyKey)
+        {
+            if (Input.GetKey("right"))
+            {
+                animator.SetTrigger("WalkRight");
+                rigidBody.velocity = new Vector2(3, rigidBody.velocity.y);
+            }
+
+            if (Input.GetKey("left"))
+            {
+                animator.SetTrigger("WalkLeft");
+                rigidBody.velocity = new Vector2(-3, rigidBody.velocity.y);
+            }
+
+            if (Input.GetKey("up"))
+            {
+                animator.SetTrigger("WalkUp");
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 3);
+            }
+
+            if (Input.GetKey("down"))
+            {
+                animator.SetTrigger("WalkDown");
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, -3);
+            }
+        }
+
+        else
+            animator.SetTrigger("NoKey");
+    }
+}
